dc99cee3e32373529031ad1328e59b5b62208290
Frady Wang <immafrady@gmail.com>
2024-01-31 23:00:07 +0800

    feat: question.go - 增加问题类型

--- internal/model/question.go
+++ internal/model/question.go
@@ -4,12 +4,21 @@ import (
 	"gorm.io/gorm"
 )
 
+type QuestionType string
+
+const (
+	QuestionMultiple QuestionType = "Multi"  // 多选
+	QuestionSingle   QuestionType = "Single" // 单选
+	QuestionJudge    QuestionType = "Judge"  // 判断
+)
+
 // Question 问题
 type Question struct {
 	gorm.Model
-	Classify Classify // 关联的课题分类
-	Q        string   // 问题
-	A        string   // 答案
-	Detail   string   // 问题补充
-	Like     bool     // 是否标记（todo 不知道bool类型支不支持）
+	Classify Classify     // 关联的课题分类
+	Q        string       // 问题
+	A        string       // 答案
+	Detail   string       // 问题补充
+	Like     bool         // 是否标记（todo 不知道bool类型支不支持）
+	Type     QuestionType // 问题类型
 }
