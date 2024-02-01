e635b738857bf88ef1de0dc14f6f00c4a3976dda
Frady Wang <immafrady@gmail.com>
2024-02-01 15:04:55 +0800

    feat: 调整数据库model，增加计数字段

--- internal/model/question.go
+++ internal/model/question.go
@@ -16,11 +16,12 @@ const (
 type Question struct {
 	gorm.Model
 	ClassifyId uint         `gorm:"foreignKey:id"` // 关联的课题分类
-	Q          string       `gorm:"column:q"`      // 问题
-	A          string       `gorm:"column:a"`      // 答案
-	Detail     string       `gorm:"column:detail"` // 问题补充
-	Like       bool         `gorm:"column:like"`   // 是否标记（todo 不知道bool类型支不支持）
-	Type       QuestionType `gorm:"column:type"`   // 问题类型
+	Q          string       // 问题
+	A          string       // 答案
+	Detail     string       // 问题补充
+	Like       bool         // 是否标记（todo 不知道bool类型支不支持）
+	Count      uint         // 做的次数
+	Type       QuestionType `gorm:"column:type"` // 问题类型
 }
 
 func (q Question) TableName() string {
