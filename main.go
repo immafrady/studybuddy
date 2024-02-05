82a6d2cb9d4346fd3109cc4e7eb7fa7cce7ad801
Frady Wang <immafrady@gmail.com>
2024-02-05 18:25:56 +0800

    feat: 把cobra弄回来

--- main.go
+++ main.go
@@ -4,12 +4,11 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
-	"github.com/immafrady/studybuddy/internal/dispatcher"
+	"github.com/immafrady/studybuddy/cmd"
 	"github.com/immafrady/studybuddy/internal/startup"
 )
 
 func main() {
 	startup.Bootstrap()
-	dispatcher.Dispatch()
-
+	cmd.Execute()
 }
