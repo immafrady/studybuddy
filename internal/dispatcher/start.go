ac38a9542fa0bea1b2945c8bfbfccf6a3581855f
Frady Wang <immafrady@gmail.com>
2024-02-06 01:33:56 +0800

    feat: 增加上下文概念

--- internal/dispatcher/start.go
+++ internal/dispatcher/start.go
@@ -1,21 +1,29 @@
 package dispatcher
 
 import (
-	"fmt"
 	"github.com/immafrady/studybuddy/internal/features/examiner"
 	"github.com/immafrady/studybuddy/internal/screens"
 	"github.com/immafrady/studybuddy/internal/service/quiz"
 	"gorm.io/gorm"
 )
 
-func start() {
-	classify := screens.ClassifyRun()
-	fmt.Printf("您选择的类目是：%v\n", classify.Name)
-	types := screens.QuestionTypeScreen()
-	limit := screens.LimitScreen()
-	quiz.FetchQuestionList(&classify, func(db *gorm.DB) *gorm.DB {
-		return db.Where("type IN (?)", types).Order("count").Limit(limit)
+func start(ctx *Context) {
+	if ctx.classify == nil {
+		classify := screens.ClassifyRun()
+		ctx.classify = &classify
+	}
+	if ctx.types == nil {
+		ctx.types = screens.QuestionTypeScreen()
+	}
+	if ctx.limit == 0 {
+		ctx.limit = screens.LimitScreen()
+	}
+	quiz.FetchQuestionList(ctx.classify, func(db *gorm.DB) *gorm.DB {
+		return db.Where("type IN (?)", ctx.types).Order("count").Limit(ctx.limit)
 	})
-	exam := examiner.NewExamHolder(&classify, types, limit)
+	exam := examiner.NewExamHolder(ctx.classify, ctx.types, ctx.limit)
 	exam.Start()
+
+	// todo 到时候判断一个布尔值，如果是true就调用
+	doOver(ctx, start)
 }
